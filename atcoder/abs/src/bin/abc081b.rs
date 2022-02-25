dd21bcc8e376307381a57d1c574254c65b156626
Ayato <ay4toh5i@gmail.com>
2022-02-26 01:46:08 +0900

    Add answer of abc081b

--- atcoder/abs/src/bin/abc081b.rs
+++ atcoder/abs/src/bin/abc081b.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+    }
+
+    input! {
+        a: [i32; n],
+    }
+
+    let result = a.iter().map(|num| num.trailing_zeros()).min().unwrap();
+
+    println!("{}", result);
 }

06b28d6ea435217921d2fb08f0a5c181788d9ce2
Ayato <ay4toh5i@gmail.com>
2022-02-27 12:31:58 +0900

    Add answer of abc087b

--- atcoder/abs/src/bin/abc087b.rs
+++ atcoder/abs/src/bin/abc087b.rs
@@ -1,3 +1,33 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: i32,
+    }
+
+    input! {
+        b: i32,
+    }
+
+    input! {
+        c: i32,
+    }
+
+    input! {
+        x: i32,
+    }
+
+    let mut ans = 0;
+
+    for i in 0..=a {
+        for j in 0..=b {
+            for k in 0..=c {
+                if 500 * i + 100 * j + 50 * k == x {
+                    ans += 1;
+                }
+            }
+        }
+    }
+
+    print!("{}", ans);
 }

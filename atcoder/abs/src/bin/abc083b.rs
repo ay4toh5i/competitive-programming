aff702881b9a9cef28479f39f96ca708e65d1bd0
Ayato <ay4toh5i@gmail.com>
2022-02-27 13:25:27 +0900

    Add answer of abc083b

--- atcoder/abs/src/bin/abc083b.rs
+++ atcoder/abs/src/bin/abc083b.rs
@@ -1,3 +1,30 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        a: i32,
+        b: i32,
+    }
+
+    let ans = (0..=n)
+        .filter(|n| {
+            let sum = sum_of_digits(*n);
+            a <= sum && sum <= b
+        })
+        .sum::<i32>();
+
+    println!("{}", ans);
+}
+
+fn sum_of_digits(n: i32) -> i32 {
+    let mut num = n;
+    let mut sum = 0;
+
+    while num > 0 {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    sum
 }

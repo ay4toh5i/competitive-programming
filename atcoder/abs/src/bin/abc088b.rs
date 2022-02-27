e74eb605f50604c958a6ab0bc1e562f56269eeb1
Ayato <ay4toh5i@gmail.com>
2022-02-27 21:34:45 +0900

    Add answer of abc088b

--- atcoder/abs/src/bin/abc088b.rs
+++ atcoder/abs/src/bin/abc088b.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+    }
+
+    input! {
+        mut a: [i32; n],
+    }
+
+    a.sort_by(|x, y| x.cmp(y).reverse());
+
+    let mut alice = 0;
+    let mut bob = 0;
+
+    a.iter().enumerate().for_each(|(i, x)| {
+        if i % 2 == 0 {
+            alice += x;
+        } else {
+            bob += x;
+        }
+    });
+
+    println!("{}", alice - bob);
 }

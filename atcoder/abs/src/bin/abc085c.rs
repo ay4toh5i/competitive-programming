15dd27c85e2fba929a8158c2fb37744369098490
Ayato <ay4toh5i@gmail.com>
2022-04-25 16:14:26 +0900

    Add answer of abc085c

--- atcoder/abs/src/bin/abc085c.rs
+++ atcoder/abs/src/bin/abc085c.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        a: i32,
+    }
+
+    for x in 0..=n {
+        for y in 0..=(n - x) {
+            let z = n - x - y;
+
+            if 10000 * x + 5000 * y + 1000 * z == a {
+                return println!("{} {} {}", x, y, z);
+            }
+        }
+    }
+
+    println!("-1 -1 -1");
 }

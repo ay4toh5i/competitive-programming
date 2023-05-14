b61eb06fd53334bb0fb6afdc8c09b2f4d92ad757
Ayato <ay4toh5i@gmail.com>
2023-05-14 21:19:18 +0900

    Add an answer of abc301a

--- atcoder/abc301/src/bin/a.rs
+++ atcoder/abc301/src/bin/a.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        _n: u32,
+        s: Chars,
+    }
+
+    let mut t_win = 0;
+    let mut a_win = 0;
+    let mut t_win_early = false;
+
+    s.iter().for_each(|c| {
+        match c {
+            'T' => t_win += 1,
+            'A' => a_win += 1,
+            _ => (),
+        }
+
+        if t_win > a_win {
+            t_win_early = true;
+        }
+    });
+
+    if t_win > a_win {
+        println!("T");
+    } else if a_win > t_win {
+        println!("A");
+    } else if s.last().unwrap() == &'T' {
+        println!("A");
+    } else {
+        println!("T");
+    }
 }

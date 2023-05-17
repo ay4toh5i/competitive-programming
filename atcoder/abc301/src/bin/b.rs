da43eee6476a843c393a6b509fd6ccfd8be487fc
Ayato <ay4toh5i@gmail.com>
2023-05-17 22:40:27 +0900

    Add an answer of abc301b

--- atcoder/abc301/src/bin/b.rs
+++ atcoder/abc301/src/bin/b.rs
@@ -1,3 +1,34 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [u32; n],
+    }
+
+    let mut result: Vec<u32> = Vec::new();
+
+    a.iter().enumerate().for_each(|(i, current)| {
+        if i + 1 >= a.len() {
+            result.push(*current);
+            return;
+        }
+
+        let next = a[i+1];
+
+        if (next > *current && next - *current == 1) || (*current > next && *current - next == 1) {
+            result.push(*current);
+            return;
+        }
+
+        let mut arr = if next > *current { 
+            (*current..next).collect::<Vec<u32>>() 
+        } else {
+            (next+1..=*current).rev().collect::<Vec<u32>>()
+        };
+
+        result.append(&mut arr);
+    });
+
+    println!("{}", result.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(" "));
 }

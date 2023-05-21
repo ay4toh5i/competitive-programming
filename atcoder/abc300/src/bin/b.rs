997ec8d2f72415e91c94bf8d1b1ccf338b625ed0
Ayato <ay4toh5i@gmail.com>
2023-05-21 19:23:09 +0900

    Add an answer of abc300b

--- atcoder/abc300/src/bin/b.rs
+++ atcoder/abc300/src/bin/b.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+        a: [Chars; h],
+        b: [Chars; h],
+    }
+
+    for i in 0..=h {
+        for j in 0..=w {
+            if a == shift(&b, i, j) {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+
+    println!("No");
+}
+
+fn shift(map: &Vec<Vec<char>>, v_shift: usize, h_shift: usize) -> Vec<Vec<char>> {
+    let _h = map.len();
+    let _w = map[0].len();
+
+    let mut result = vec![vec!['x'; _w]; _h];
+
+    for i in 0.._h {
+        for j in 0.._w {
+            result[i][j] = map[(i + v_shift) % _h][(j + h_shift) % _w];
+        }
+    }
+
+    result
 }

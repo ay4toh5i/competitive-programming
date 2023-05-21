3f7a85db56f5af157bff164b922ab4b6a307092f
Ayato <ay4toh5i@gmail.com>
2023-05-21 17:28:02 +0900

    Add an answer of abc300a

--- atcoder/abc300/src/bin/a.rs
+++ atcoder/abc300/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: u32,
+        a: u32,
+        b: u32,
+        answers: [u32; n],
+    }
+
+    let sum = a + b;
+
+    let index = answers.iter().position(|&x| x == sum).unwrap();
+
+    println!("{}", index + 1);
 }

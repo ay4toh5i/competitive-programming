4d37f4d101719b56f1df230e7c497ae971ef51f2
Ayato <ay4toh5i@gmail.com>
2023-05-24 20:09:33 +0900

    Add an answer of abc299b

--- atcoder/abc299/src/bin/b.rs
+++ atcoder/abc299/src/bin/b.rs
@@ -1,3 +1,34 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        t: usize,
+        c: [usize; n],
+        r: [usize; n],
+    }
+
+    let f = c[0];
+
+    let mut t_win: Option<usize> = None;
+    let mut f_win: Option<usize> = None;
+
+    for (i, (&a, &b)) in c.iter().zip(&r).enumerate() {
+        if a == t && (t_win == None || b > r[t_win.unwrap()]) {
+            t_win = Some(i);
+        }
+
+        if a == f && (f_win == None || b > r[f_win.unwrap()]) {
+            f_win = Some(i);
+        }
+    }
+
+    if let Some(x) = t_win {
+        println!("{}", x  + 1);
+        return;
+    } 
+
+    if let Some(y) = f_win {
+        println!("{}", y + 1);
+    }
 }

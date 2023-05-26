66c5136900600080a21c778f852f186b4fd1e846
Ayato <ay4toh5i@gmail.com>
2023-05-26 23:48:49 +0900

    Add an answer of abc298a

--- atcoder/abc298/src/bin/a.rs
+++ atcoder/abc298/src/bin/a.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        s: Chars,
+    }
+
+    let mut r = 0;
+    let mut f = 0;
+
+    for &x in s.iter() { 
+        if x == 'o' {
+            r += 1;
+        }
+
+        if x == 'x' {
+            f += 1;
+        }
+    }
+
+    if f == 0 && r > 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }

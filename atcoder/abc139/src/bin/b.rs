3a67a8e9851c791ae5bd7c607759a44c462453c8
Ayato <ay4toh5i@gmail.com>
2023-05-27 18:01:32 +0900

    Add an answer of abc139b

--- atcoder/abc139/src/bin/b.rs
+++ atcoder/abc139/src/bin/b.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+    };
+
+    // コンセントをb-1増やす、電源タップひとつでa-1個増やせる
+    // (b-1) / (a-1) の切り上げ
+    println!("{}", (b - 1 + a - 2) / (a - 1));
 }

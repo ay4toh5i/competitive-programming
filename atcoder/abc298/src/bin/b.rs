0d561360121ab3aa845ee94165e5074a64e1a758
Ayato <ay4toh5i@gmail.com>
2023-05-27 17:38:31 +0900

    Add an answer of abc298b

--- atcoder/abc298/src/bin/b.rs
+++ atcoder/abc298/src/bin/b.rs
@@ -1,3 +1,45 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [[usize; n]; n],
+        b: [[usize; n]; n],
+    };
+
+    let mut _a = a;
+
+    for _x in 0..4 {
+        let mut ok = true;
+
+        for i in 0.._a.len() {
+            for j in 0.._a.len() {
+                if _a[i][j] == 1 && b[i][j] == 0 {
+                    ok = false;
+                }
+            }
+        }
+
+        if ok {
+            println!("Yes");
+            return;
+        }
+        
+        _a = rot(_a);
+    }
+
+    println!("No");
+}
+
+fn rot(m: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let size = m.len();
+    let mut result = vec![vec![0; size]; size];
+
+    for i in 0..size {
+        for j in 0..size {
+            result[i][j] = m[size-j-1][i];
+        }
+    }
+
+    result
 }

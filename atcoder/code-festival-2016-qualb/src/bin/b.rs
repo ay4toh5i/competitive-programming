a447e52c60865397240dd8f9786b6352a012f8a2
Ayato <ay4toh5i@gmail.com>
2023-05-28 15:47:02 +0900

    Add an answer of cf2016qualb/b

--- atcoder/code-festival-2016-qualb/src/bin/b.rs
+++ atcoder/code-festival-2016-qualb/src/bin/b.rs
@@ -1,3 +1,30 @@
+use proconio::fastout;
+use proconio::input;
+use proconio::marker::Chars;
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        a: usize,
+        b: usize,
+        s: Chars,
+    };
+
+    let mut accepted = 0;
+
+    let mut accepted_b = 0;
+
+    for &x in s.iter() {
+        if x == 'a' && accepted < a + b {
+            accepted += 1;
+            println!("Yes");
+        } else if x == 'b' && accepted < a + b && accepted_b < b {
+            accepted += 1;
+            accepted_b += 1;
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
 }

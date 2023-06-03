a45b6cc3fc004a5f42c911db9b03c4b301fbd48c
Ayato <ay4toh5i@gmail.com>
2023-06-03 16:54:01 +0900

    Add an answer of panasonic2020b

--- atcoder/panasonic2020/src/bin/b.rs
+++ atcoder/panasonic2020/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+    };
+
+    if h == 1 || w == 1 {
+        println!("1");
+        return;
+    }
+
+    let even_col_point = h / 2;
+    let odd_col_point = h - even_col_point;
+
+    let even_col_num = w / 2;
+    let odd_col_num = w - even_col_num;
+
+    println!("{}", even_col_point * even_col_num + odd_col_point * odd_col_num);
 }

c4137dadb2a27dbaa14016a3e2bfe2f36a7a7229
Ayato <ay4toh5i@gmail.com>
2023-06-03 16:23:59 +0900

    Add an answer of sumitrust2019b

--- atcoder/sumitrust2019/src/bin/b.rs
+++ atcoder/sumitrust2019/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
+// https://img.atcoder.jp/sumitrust2019/editorial.pdf
 fn main() {
-    todo!();
+    input! {
+        n: usize
+    };
+
+    let x = ((n as f64) / 1.08).ceil();
+
+    if (x * 1.08).floor() as usize == n {
+        println!("{}", x);
+    } else {
+        println!(":(");
+    }
 }

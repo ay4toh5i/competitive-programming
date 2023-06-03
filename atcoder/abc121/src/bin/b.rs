6a168419a3dcadbd7ff5deae4b5801a3b8fd27f0
Ayato <ay4toh5i@gmail.com>
2023-06-03 16:35:05 +0900

    Add an answer of abc121b

--- atcoder/abc121/src/bin/b.rs
+++ atcoder/abc121/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        c: isize,
+        b: [isize; m],
+        a: [[isize; m]; n],
+    };
+
+    let result = a.iter().filter(|&x| {
+        let s = x.iter().enumerate().map(|(i, &y)| y * b[i]).sum::<isize>() + c;
+
+        s > 0
+    }).count();
+
+    println!("{}", result);
 }

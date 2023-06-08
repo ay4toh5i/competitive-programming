e714c729e7dcab54091e34072b5244a4a044539d
Ayato <ay4toh5i@gmail.com>
2023-06-08 22:19:18 +0900

    Add an answer of abc086

--- atcoder/abc086/src/bin/b.rs
+++ atcoder/abc086/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: String,
+        b: String,
+    };
+
+    let n: usize = format!("{}{}", a ,b).parse().unwrap();
+
+    for x in (0..(n / 2)).rev() {
+        if x.pow(2) == n {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }

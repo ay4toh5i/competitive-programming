6cb0d33716686f976c167e1927ea7ddd9210e1a7
Ayato <ay4toh5i@gmail.com>
2023-06-08 22:04:21 +0900

    Add an answer of abc157

--- atcoder/abc157/src/bin/b.rs
+++ atcoder/abc157/src/bin/b.rs
@@ -1,3 +1,48 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: [[usize; 3]; 3],
+        n: usize,
+        b: [usize; n],
+    };
+
+    let mut matrix = vec![vec![false; 3]; 3];
+
+    'outer: for &v in b.iter() {
+        for (i, r) in a.iter().enumerate() {
+            for (j, &c) in r.iter().enumerate() {
+                if v == c {
+                    matrix[i][j] = true;
+                    continue 'outer;
+                }
+            }
+        }
+    }
+
+    for x in 0..3 {
+        if matrix[x].iter().all(|&v| v) {
+            println!("Yes");
+            return;
+        }
+    }
+
+    for x in 0..3 {
+        if (0..3).map(|v| matrix[v][x]).all(|v| v) {
+            println!("Yes");
+            return;
+        }
+    }
+
+    if (0..3).map(|v| matrix[v][v]).all(|v| v) {
+        println!("Yes");
+        return;
+    }
+
+    if (0..3).map(|v| matrix[v][2 - v]).all(|v| v) {
+        println!("Yes");
+        return;
+    }
+
+    println!("No");
 }

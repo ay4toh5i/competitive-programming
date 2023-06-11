718ba39f760529ed212ed699b5c14ad6a40caaec
Ayato <ay4toh5i@gmail.com>
2023-06-11 15:50:42 +0900

    Add an answer of abc068

--- atcoder/abc068/src/bin/b.rs
+++ atcoder/abc068/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    };
+
+    if n < 2 {
+        println!("{}", n);
+        return;
+    }
+
+    let mut result: usize = 2;
+
+    loop {
+        if result * 2 > n {
+            println!("{}", result);
+            return;
+        }
+
+        result *= 2;
+    }
 }

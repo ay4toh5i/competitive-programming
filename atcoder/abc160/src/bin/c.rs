d2903bb39088dfd51566f913aab4f6049cfd3a70
Ayato <ay4toh5i@gmail.com>
2023-06-11 16:19:10 +0900

    Add an answer of abc160c

--- atcoder/abc160/src/bin/c.rs
+++ atcoder/abc160/src/bin/c.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        k: usize,
+        n: usize,
+        a: [usize; n],
+    };
+
+    let mut distances: Vec<usize> = vec![];
+
+    for i in 0..(a.len() - 1) {
+        distances.push(a[i + 1] - a[i]);
+    }
+
+    distances.push(a[0] + k - a[a.len() - 1]);
+
+    let result = distances.iter().sum::<usize>() - distances.iter().max().unwrap();
+
+    println!("{}", result);
 }

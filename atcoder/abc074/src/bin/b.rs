76c026f505f6aa2abed8c4a877c0ac90e2ba5416
Ayato <ay4toh5i@gmail.com>
2023-06-11 15:20:53 +0900

    Add an answer of abc074b

--- atcoder/abc074/src/bin/b.rs
+++ atcoder/abc074/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        x: [usize; n],
+    };
+
+    let result: usize = x.iter().map(|&v| {
+        let d = if k - v < v { k -v } else { v };
+        d * 2
+    }).sum();
+
+    println!("{}", result);
 }
